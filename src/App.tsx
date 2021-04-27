56b460d0ad85884fd702456d9563ff695505c051
Ryohei Tokuda <tokuda@idein.jp>
2021-04-27 15:58:31 +0900

    Implement standard player

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { render } from "react-dom";
+
+import VideoPlayer from "./videoplayer";
+
+const videoJsOptions = {
+  autoplay: true,
+  controls: true,
+  sources: [
+    {
+      src: "http://vjs.zencdn.net/v/oceans.mp4",
+      type: "video/mp4",
+    },
+  ],
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <VideoPlayer { ...videoJsOptions } />
 }
-
 export default App;
---
+++ src/videoplayer.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+
+export default class VideoPlayer extends React.Component {
+  player: any;
+  videoNode: any;
+  props: any;
+
+  componentDidMount() {
+    // instantiate Video.js
+    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+      console.log("onPlayerReady");
+    });
+  }
+
+  // destroy player on unmount
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+
+  // wrap the player in a div with a `data-vjs-player` attribute
+  // so videojs won't create additional wrapper in the DOM
+  // see https://github.com/videojs/video.js/pull/3856
+  render() {
+    return (
+      <div>
+        <div data-vjs-player>
+          <video
+            ref={(node) => (this.videoNode = node)}
+            className="video-js"
+          ></video>
+        </div>
+      </div>
+    );
+  }
+}
